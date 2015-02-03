5e17c1c1cce4b9b95698e2b92020105a6423c1f9
Alex Nichol <unixpickle@gmail.com>
2015-02-03 14:28:03 -0500

    started working on Scanner

---
+++ scanner.go
@@ -0,0 +1,87 @@
+package pragmash
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"unicode"
+)
+
+// A Scanner reads specific kinds of tokens from a io.RuneScanner.
+type Scanner struct {
+	io.RuneScanner
+}
+
+// NewScannerString creates a scanner which operates on a string.
+func NewScannerString(str string) Scanner {
+	return Scanner{bytes.NewBufferString(str)}
+}
+
+// ReadBare reads a bareword, supporting escapes and terminating at a space or
+// an EOF.
+func (s Scanner) ReadBare() (string, error) {
+	next, _, err := s.ReadRune()
+	res := ""
+	for err != nil {
+		if unicode.IsSpace(next) {
+			s.UnreadRune()
+			break
+		} else if next == '\\' {
+			x, err := s.ReadEscape()
+			if err != nil {
+				return "", err
+			}
+			res += x
+		} else {
+			res += string(next)
+		}
+		next, _, err = s.ReadRune()
+	}
+	return res, nil
+}
+
+// ReadEscape reads escape characters and returns the represented string.
+// This does not read a '\'; such a character should already have been read.
+func (s Scanner) ReadEscape() (string, error) {
+	// TODO: support hex escapes
+	next, _, err := s.ReadRune()
+	if err != nil {
+		return "", errors.New("failed to read escape code")
+	}
+	if next == 'n' {
+		return "\n", nil
+	} else if next == 'r' {
+		return "\r", nil
+	} else if next == 'a' {
+		return "\a", nil
+	} else if next == 't' {
+		return "\t", nil
+	}
+	return string(next), nil
+}
+
+// SkipLine reads up to and including the next newline.
+func (s Scanner) SkipLine() error {
+	next, _, err := s.ReadRune()
+	for err == nil {
+		if next == '\n' {
+			return nil
+		}
+		next, _, err = s.ReadRune()
+	}
+	return err
+}
+
+// SkipWhitespace reads up to but not including the next non-whitespace
+// character.
+func (s Scanner) SkipWhitespace() error {
+	next, _, err := s.ReadRune()
+	for err == nil {
+		if !unicode.IsSpace(next) {
+			s.UnreadRune()
+			return nil
+		}
+		next, _, err = s.ReadRune()
+	}
+	return err
+}
--- tokenizer.go
+++ tokenizer.go
@@ -1,9 +1,43 @@
 package pragmash
 
+import (
+	"strings"
+)
+
 type Line struct {
 	CloseBlock bool
 	OpenBlock  bool
 	Tokens     []string
 }
 
-func TokenizeLine(line string)
+type Tokenizer struct {
+	previous string
+}
+
+// NewTokenizer returns a tokenizer with an empty buffer.
+func NewTokenizer() *Tokenizer {
+	return &Tokenizer{}
+}
+
+// Done returns true if the tokenizer has no text waiting to be processed.
+func (t *Tokenizer) Done() bool {
+	return len(t.previous) == 0
+}
+
+// Line takes a line as a string and processes it.
+//
+// If the line was the end of a logical token expression, such expression is
+// returned as the first return parameter.
+//
+// If the line needs more data to be completed (i.e. ends with a backslash)
+// then this returns a nil *Line and a nil error.
+func (t *Tokenizer) Line(line string) (*Line, error) {
+	full := t.previous + line
+	if strings.HasSuffix(line, "\\") {
+		t.previous = full
+		return nil, nil
+	}
+	// TODO: tokenize the line here
+	return nil, nil
+}
+
