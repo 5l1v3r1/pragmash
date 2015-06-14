830488e7d7344d6a60ca332bdef9ccfd0c36748f
Alex Nichol <unixpickle@gmail.com>
2015-06-14 14:24:06 -0400

    i am no longer a slave to the 80 character limit

--- lines.go
+++ lines.go
@@ -7,11 +7,10 @@ import (
 	"strings"
 )
 
-// A LineReader delivers a continual stream of lines until an EOF is
-// encountered.
+// A LineReader delivers a continual stream of lines until an EOF is encountered.
 //
-// The ReadLine method will return a line and a line number. If an error occurs,
-// the error will be non-nil and the other two return values should be ignored.
+// The ReadLine method will return a line and a line number.
+// If an error occurs, the error will be non-nil and the other two return values should be ignored.
 // If the error is io.EOF, the LineReader has finished reading gracefully.
 type LineReader interface {
 	ReadLine() (string, int, error)
@@ -23,15 +22,14 @@ type PhysLineReader struct {
 	lineNumber int
 }
 
-// NewPhysLineReader creates a PhysLineReader which acts on a given io.Reader
-// and starts at line 1.
+// NewPhysLineReader creates a PhysLineReader which acts on a given io.Reader and starts at line 1.
 func NewPhysLineReader(r io.Reader) *PhysLineReader {
 	return &PhysLineReader{bufio.NewReader(r), 0}
 }
 
-// ReadLine reads the next physical line and returns both the line itself and
-// the corresponding line number. If an error occurs, it returns a non-nil
-// error and the other return values should be ignored.
+// ReadLine reads the next physical line and returns both the line itself and the corresponding
+// line number.
+// If an error occurs, it returns a non-nil error and the other return values should be ignored.
 func (p *PhysLineReader) ReadLine() (string, int, error) {
 	result := new(bytes.Buffer)
 	for {
@@ -51,18 +49,17 @@ func (p *PhysLineReader) ReadLine() (string, int, error) {
 	return strings.TrimSuffix(result.String(), "\r"), p.lineNumber, nil
 }
 
-// A LogicalLineReader wraps a LineReader and does extra processing to remove
-// whitespace and handle line continuations.
+// A LogicalLineReader wraps a LineReader and does extra processing to remove whitespace and handle
+// line continuations.
 //
-// Line numbers from the underlying LineReader are preserved. The first
-// underlying line number for the logical line is returned as the logical line
-// number.
+// Line numbers from the underlying LineReader are preserved.
+// The first underlying line number for the logical line is returned as the logical line number.
 type LogicalLineReader struct {
 	Reader LineReader
 }
 
-// ReadLine reads one or more physical lines and returns a logical line or an
-// error.
+// ReadLine reads one or more physical lines from its underlying reader and returns a logical line
+// or an error.
 func (l LogicalLineReader) ReadLine() (string, int, error) {
 	result := new(bytes.Buffer)
 	firstLineNum := -1
